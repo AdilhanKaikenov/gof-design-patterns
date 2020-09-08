b6bfbbc6faa7125aaa7ff7d128fb26270d5ee033
AdilhanKaikenov <adilhan_kai@mail.ru>
2020-09-08 17:39:49 +0600

    Bridge design pattern

---
+++ bridge/RREADME.md
@@ -0,0 +1,29 @@
+
+An example of a problem:  
+
+![alt text](./etc/bridge.jpg "Bridge class diagram")
+
+If in the future it will be necessary to add the Tractor interface, 
+then for each model (Audi, Mercedes, Toyota) it will be necessary to create a separate implementation class, 
+or if we need to add a new model and the number of classes will begin to increase rapidly.
+
+```java 
+interface Car {}
+interface Track {}
+interface Bike {}
+
+class AudiCar implements Car {}
+class AudiTrack implements Track {}
+class AudiBike implements Bike {}
+
+class MercedesCar implements Car {}
+class MercedesTrack implements Track {}
+class MercedesBike implements Bike {}
+
+class ToyotaCar implements Car {}
+class ToyotaTrack implements Track {}
+class ToyotaBike implements Bike {}
+
+```
+
+![alt text](./etc/bridge-uml.jpg "Bridge class diagram")
---
+++ bridge/src/BridgeExample.java
@@ -0,0 +1,75 @@
+/**
+ * When adding a new vehicle or model, you don't have to add new implementation classes.
+ *
+ * Wikipedia says
+ *
+ * The bridge pattern is a design pattern used in software engineering that is meant to
+ * "decouple an abstraction from its implementation so that the two can vary independently"
+ */
+public class BridgeExample {
+    public static void main(String[] args) {
+
+        Vehicle vehicle1 = new Car(new Toyota());
+        vehicle1.drive();
+
+        Vehicle vehicle2 = new Track(new Audi());
+        vehicle2.drive();
+    }
+}
+
+// Транспортное средство
+abstract class Vehicle {
+
+    Model model;
+
+    public Vehicle(final Model model) {
+        this.model = model;
+    }
+
+    abstract void drive();
+}
+
+class Car extends Vehicle {
+
+    public Car(final Model model) {
+        super(model);
+    }
+
+    @Override
+    void drive() {
+        model.drive("Drive car");
+    }
+}
+
+class Track extends Vehicle {
+
+    public Track(final Model model) {
+        super(model);
+    }
+
+    @Override
+    void drive() {
+        model.drive("Drive track");
+    }
+}
+
+// The interface which is the vehicle model
+interface Model {
+    void drive(String str);
+}
+
+class Audi implements Model {
+
+    @Override
+    public void drive(final String str) {
+        System.out.println(str + " (Audi)");
+    }
+}
+
+class Toyota implements Model {
+
+    @Override
+    public void drive(final String str) {
+        System.out.println(str + " (Toyota)");
+    }
+}
